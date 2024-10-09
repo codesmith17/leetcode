39bf523f69fa1efe82bf1a15e610e51c09ec5831
Krishna Tripathi <121955562+codesmith17@users.noreply.github.com>
2024-10-09 23:28:09 +0530

    Time: 199 ms (7.17%), Space: 76.9 MB (54.39%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int n = customers.size();
+        long long waitTime = customers[0][1];
+        int startTime = customers[0][1] + customers[0][0];
+        for (int i = 1; i < n; i++) {
+            if (startTime > customers[i][0]) {
+                waitTime += startTime - customers[i][0];
+            } else {
+                startTime = customers[i][0];
+            }
+            waitTime += customers[i][1];
+            startTime += customers[i][1];
+            cout << waitTime << endl;
+        }
+        return waitTime * 1.0 / n;
+    }
+};
