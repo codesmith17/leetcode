ea1b34a50061cbbf386cdf004381d32fd9002c65
Krishna Tripathi <121955562+codesmith17@users.noreply.github.com>
2024-10-18 09:28:01 +0530

    Time: 59 ms (34.21%), Space: 26.6 MB (17.22%) - LeetHub

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void recursion(vector<int>& nums, vector<int>& ds, int ind, int n,
+                   int& maxOR, vector<int>& res) {
+        if (ind == n) {
+            int currentOR = 0;
+            for (auto it : ds) 
+                currentOR |= it;
+            maxOR = max(maxOR, currentOR);
+            res.push_back(currentOR);
+            return;
+        }
+        ds.push_back(nums[ind]);
+        recursion(nums, ds, ind + 1, n, maxOR, res);
+
+        ds.pop_back();
+        recursion(nums, ds, ind + 1, n, maxOR, res);
+    }
+
+    int countMaxOrSubsets(vector<int>& nums) {
+        vector<int> ds, res;
+        int maxOR = 0;
+        recursion(nums, ds, 0, nums.size(), maxOR, res);
+        int cnt = 0;
+        for (auto it : res) 
+            if (it == maxOR)
+                cnt++;
+        
+        return cnt;
+    }
+};
