a69c8b4deeb41ebaf10b3d57e7931965e5a98883
Krishna Tripathi <121955562+codesmith17@users.noreply.github.com>
2024-10-18 09:34:04 +0530

    Time: 79 ms (22.22%), Space: 12.3 MB (25.25%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     void recursion(vector<int>& nums, vector<int>& ds, int ind, int n,
-                   int& maxOR, vector<int>& res) {
+                   int& maxOR, unordered_map<int, int> &mp) {
         if (ind == n) {
             int currentOR = 0;
-            for (auto it : ds) 
+            for (auto it : ds)
                 currentOR |= it;
             maxOR = max(maxOR, currentOR);
-            res.push_back(currentOR);
+            mp[currentOR]++;
             return;
         }
         ds.push_back(nums[ind]);
-        recursion(nums, ds, ind + 1, n, maxOR, res);
+        recursion(nums, ds, ind + 1, n, maxOR, mp);
 
         ds.pop_back();
-        recursion(nums, ds, ind + 1, n, maxOR, res);
+        recursion(nums, ds, ind + 1, n, maxOR, mp);
     }
 
     int countMaxOrSubsets(vector<int>& nums) {
-        vector<int> ds, res;
+        vector<int> ds;
+        unordered_map<int, int> mp;
         int maxOR = 0;
-        recursion(nums, ds, 0, nums.size(), maxOR, res);
-        int cnt = 0;
-        for (auto it : res) 
-            cnt += it == maxOR;
-        
-        return cnt;
+        recursion(nums, ds, 0, nums.size(), maxOR, mp);
+        return mp[maxOR];
     }
 };
