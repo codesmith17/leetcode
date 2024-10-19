572aec137c38f7c18c81c72f7953cb807cb78a20
Krishna Tripathi <121955562+codesmith17@users.noreply.github.com>
2024-10-19 15:18:42 +0530

    Time: 11 ms (51.54%), Space: 40.4 MB (50.68%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    string recursion(int n) {
-        if (n == 1) {
+    string recursion(int n, int k) {
+        if (n == 1)
             return "0";
-        }
-        string s = recursion(n - 1);
+        string s = recursion(n - 1, k);
+        if (s.size() > k)
+            return s;
         string inverted = s;
-        for (auto &it : inverted) {
+        for (auto& it : inverted) {
             it = (it == '0') ? '1' : '0';
         }
         reverse(inverted.begin(), inverted.end());
@@ -14,7 +15,7 @@ public:
     }
 
     char findKthBit(int n, int k) {
-        string s = recursion(n);
+        string s = recursion(n, k);
         return s[k - 1];
     }
 };
