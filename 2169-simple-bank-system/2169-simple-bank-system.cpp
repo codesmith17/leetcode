8abee7a910e63bae09c524d1a578a9be40b1d928
Krishna Tripathi <121955562+codesmith17@users.noreply.github.com>
2024-10-19 13:52:59 +0530

    Time: 16 ms (100%), Space: 118.7 MB (84.93%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.cpp
+++ 2169-simple-bank-system/2169-simple-bank-system.cpp
@@ -3,11 +3,13 @@ public:
     vector<long long>& balance;
 
     Bank(vector<long long>& balance) : balance(balance) {}
-
+    bool possible(int account) {
+        return (account <= 0 or account > balance.size());
+    }
     bool transfer(int account1, int account2, long long money) {
-        if (account1 <= 0 or account1 > balance.size() or account2 <= 0 or account2 > balance.size()) {
+        if (possible(account1) or possible(account2))
             return false;
-        }
+
         if (balance[account1 - 1] >= money) {
             balance[account1 - 1] -= money;
             balance[account2 - 1] += money;
@@ -17,17 +19,17 @@ public:
     }
 
     bool deposit(int account, long long money) {
-        if (account <= 0 or account > balance.size()) {
+        if (possible(account))
             return false;
-        }
+
         balance[account - 1] += money;
         return true;
     }
 
     bool withdraw(int account, long long money) {
-        if (account <= 0 or account > balance.size()) {
+        if (possible(account))
             return false;
-        }
+
         if (balance[account - 1] >= money) {
             balance[account - 1] -= money;
             return true;
